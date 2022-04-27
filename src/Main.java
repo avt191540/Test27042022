9a3bdf374c30930b9ddb0b5b123d5d60a906e57c
Igor <avt191540@yandex.ru>
2022-04-27 21:42:05 +0300

    Test1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! Hi!");
+        System.out.println("Hello world! Hi! O my got!");
     }
 }
